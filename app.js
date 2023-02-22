112ad6c535593f4e9e4ea06868e5244517175777
Jesus-correa <jedacoma45@gmail.com>
2023-02-22 18:08:38 -0500

    Modificaciones para Render

--- app.js
+++ app.js
@@ -9,8 +9,9 @@ const publicPath = path.resolve(__dirname, './public')
 app.use(express.static(publicPath))
 
 //Asignacion al puerto
+const port = process.env.PORT || 3000
 app.listen(3000,() => {
-    console.log("Funciona en puerto 3.000");
+    console.log("Funciona en puerto "+ port);
 });
 
 //Funcionalidad y asignacion del html raiz
